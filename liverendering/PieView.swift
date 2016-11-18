46664cb2ac5f24c0e53b9d57d95c9a3f88d1c327
Gabriel Erdelyi <erdgabios@gmail.com>
2016-11-19 00:28:21 +0100

    first commit

---
+++ liverendering/PieView.swift
@@ -0,0 +1,21 @@
+//
+//  PieView.swift
+//  liverendering
+//
+//  Created by G on 18/11/2016.
+//  Copyright © 2016 erdgabios. All rights reserved.
+//
+
+import UIKit
+
+class PieView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
