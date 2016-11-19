3732c7166bb4da185bc5b9d86a49956bebbd665f
Gabriel Erdelyi <erdgabios@gmail.com>
2016-11-19 13:41:00 +0100

    Adding the first layer to PieChart

--- liverendering/PieView.swift
+++ liverendering/PieView.swift
@@ -52,6 +52,49 @@ class PieView: UIView {
     
     func createChart() {
         
+        layoutBackgroundLayer()
+        layoutBackgroundImageLayer()
+        
+    }
+    
+    func layoutBackgroundLayer() {
+        
+        if backgroundLayer == nil {
+            backgroundLayer = CAShapeLayer()
+            layer.addSublayer(backgroundLayer)
+            
+            let rectangle = bounds.insetBy(dx: lineWidth / 2, dy: lineWidth / 2)
+            let path = UIBezierPath(ovalIn: rectangle)
+            
+            backgroundLayer.path = path.cgPath
+            backgroundLayer.fillColor = backgroundLayerColor.cgColor
+            backgroundLayer.lineWidth = lineWidth
+            backgroundLayer.frame = layer.bounds
+            
+        }
+    }
+    
+    func layoutBackgroundImageLayer() {
+        
+        if backgroundImageLayer == nil {
+            let imageMask = CAShapeLayer()
+            
+            let inset = lineWidth + 3
+            let insetBounds = self.bounds.insetBy(dx: inset, dy: inset)
+            let maskPath = UIBezierPath(ovalIn: insetBounds)
+            imageMask.path = maskPath.cgPath
+            imageMask.fillColor = UIColor.black.cgColor
+            imageMask.frame = self.bounds
+            
+            backgroundImageLayer = CAShapeLayer()
+            backgroundImageLayer.mask = imageMask
+            backgroundImageLayer.frame = self.bounds
+            backgroundImageLayer.contentsGravity = kCAGravityResizeAspectFill
+            layer.addSublayer(backgroundImageLayer)
+            
+            
+            
+        }
     }
     
     
@@ -62,6 +105,12 @@ class PieView: UIView {
     
     
     func updateLayerProperties() {
+        
+        if backgroundImageLayer != nil {
+            if let image = backgroundImage {
+                backgroundImageLayer.contents = image.cgImage
+            }
+        }
         
     }
 
